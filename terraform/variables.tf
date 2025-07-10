b8e3d5ff0370108434380d710934233b414c64c9
eshanmalyala <eshanmalyala@outlook.com>
2025-07-10 23:00:02 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "project_id" {
+  default = "gcp-agent-garden"
+}
+
+variable "region" {
+  default = "europe-west1"
+}
+
+variable "bucket_name" {
+  default = "getwellsoon-bucket-demo"
+}
+
+variable "dataset_id" {
+  default = "getwellsoon_dataset"
+}
+
+variable "topic_id" {
+  default = "getwellsoon-topic"
+}
+
+variable "artifact_registry_name" {
+  default = "streaming-registry"
+}
